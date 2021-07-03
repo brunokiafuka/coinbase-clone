3e94070420d4ffac39db3072b152d0f6baeb016f
Bruno Kiafuka <brunokiafuka@live.com.pt>
2021-07-03 03:37:50 +0200

    ✨  common styles constant

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,7 +1,8 @@
-const tintColorLight = '#2f95dc';
+const tintColorLight = '#0B46E4';
 const tintColorDark = '#fff';
 
 export default {
+  darkText: '#61758E',
   light: {
     text: '#000',
     background: '#fff',
---
+++ constants/CommonStyles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+import Colors from './Colors';
+
+export default StyleSheet.create({
+  flexEnd: { alignItems: 'flex-end' },
+  flexOneRow: { flex: 1, marginHorizontal: 10 },
+  darkText: { color: Colors.darkText },
+});
